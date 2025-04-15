74455b8192b2258d3c7ee0ed7f6ba21b59339891
xveqc3204 <c27john.alves@afacademy.af.edu>
2025-04-15 16:11:32 -0600

    Final Lab 4 Code

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -25,8 +25,14 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is
 
     -- signal declarations
+    signal w_floor1 : STD_LOGIC_VECTOR (3 downto 0);	
+    signal w_floor2 : STD_LOGIC_VECTOR (3 downto 0);	
+    signal w_data : STD_LOGIC_VECTOR (3 downto 0);	
+    signal w_reset : STD_LOGIC;
+    signal w_reset_clk : STD_LOGIC;
+    signal w_clk1 : std_logic;	
+    signal w_clk2 : std_logic;	
     
-  
 	-- component declarations
     component sevenseg_decoder is
         port (
@@ -69,15 +75,75 @@ architecture top_basys3_arch of top_basys3 is
     end component clock_divider;
 	
 begin
+
 	-- PORT MAPS ----------------------------------------
-    	
+    clock_divider_1 : clock_divider
+	generic map(
+	k_DIV => 50000000 
+	)
+	port map (
+        i_clk => clk,
+        i_reset => w_reset_clk,
+        o_clk => w_clk1
+	);
 	
+    clock_divider_2 : clock_divider
+	generic map(
+	k_DIV => 100000 
+	)
+	port map (
+        i_clk => clk,
+        i_reset => w_reset_clk,
+        o_clk => w_clk2
+	);
+	
+    elevator_controller_fsm_1 : elevator_controller_fsm
+    port map(
+        i_clk => w_clk1,
+        i_reset => w_reset,
+        is_stopped => sw(0), 
+        go_up_down => sw(1),
+        o_floor => w_floor1  
+    );
+	
+	elevator_controller_fsm_2 : elevator_controller_fsm
+    port map(
+        i_clk => w_clk1,
+        i_reset => w_reset,
+        is_stopped => sw(14), 
+        go_up_down => sw(15),
+        o_floor => w_floor2  
+    );
+    
+	TDM4_Final : TDM4
+	port map(
+	
+       i_clk => w_clk2, 	
+       i_reset => btnU,
+       i_D3 => x"F",
+       i_D2 => w_floor2,
+       i_D1 => x"F",
+       i_D0 => w_floor1,
+       o_data => w_data,
+       o_sel => an
+	);
+   
+    sevenseg_decoder_Final : sevenseg_decoder
+    port map(
+        i_Hex => w_data,
+        o_seg_n => seg
+    );
+    
 	-- CONCURRENT STATEMENTS ----------------------------
-	
 	-- LED 15 gets the FSM slow clock signal. The rest are grounded.
-	
+    led(15) <= w_clk1;
+    led(14 downto 0) <= (others => '0');
 	-- leave unused switches UNCONNECTED. Ignore any warnings this causes.
 	
-	-- reset signals
-	
+    -- reset signals
+    -- Elevator Controller Reset Logic
+    w_reset <= btnR or btnU;
+    -- Clock Reset Logic
+    w_reset_clk <= btnL or btnU;
+    
 end top_basys3_arch;
